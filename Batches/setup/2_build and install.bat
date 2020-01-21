2e87d0f181cf02596e5d8d52148b8d6ec42bd6c2
OkamotoYuta <okamotoyuta6502@gmail.com>
2020-01-21 21:49:19 +0900

    簡単にバッチで実行できるようにバッチを追加

---
+++ Batches/setup/2_build and install.bat
@@ -0,0 +1,3 @@
+call %userprofile%\miniconda3\Scripts\activate.bat
+python setup.py build
+python setup.py install
--- spleeter_build-install.bat
+++
@@ -1,3 +0,0 @@
-call %userprofile%\Anaconda3\Scripts\activate.bat
-python setup.py build
-python setup.py install
