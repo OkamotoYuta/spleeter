ed23f0e414ce585ac0c44d2ef2fb2e02f2fbf09b
OkamotoYuta <okamotoyuta6502@gmail.com>
2020-01-21 22:58:08 +0900

    修正したバッチを追加

---
+++ batches/setup/1_install ffmpeg.bat
@@ -0,0 +1,2 @@
+call %userprofile%\miniconda3\Scripts\activate.bat
+conda install -y ffmpeg
---
+++ batches/setup/2_build and install.bat
@@ -0,0 +1,4 @@
+cd ..\..
+call %userprofile%\miniconda3\Scripts\activate.bat
+python setup.py build
+python setup.py install
